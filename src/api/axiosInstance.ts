550f047e3c4ce4d672c8b5fdbd0b8b862a38d27d
Rofi <rofisudyono@gmail.com>
2022-08-12 14:57:23 +0700

    update axios instance (#49)

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,12 +1,9 @@
 import axios from 'axios';
-import getConfig from 'next/config';
-
-const {publicRuntimeConfig} = getConfig();
 
 const instance = axios.create({
-  baseURL: `${publicRuntimeConfig.myriadAPIURL}`,
+  baseURL: `https://api.testnet.myriad.social/`,
   headers: {
-    Authorization: `Bearer ${publicRuntimeConfig.myriadAPIKey}`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNTU0NTExZTVkZTJkOGE4MTdkYzI2NyIsIm5hbWUiOiJNeXJpYWQgT2ZmaWNpYWwiLCJ1c2VybmFtZSI6Im15cmlhZF9vZmZpY2lhbCIsInBlcm1pc3Npb25zIjpbInVzZXIiLCJhZG1pbiIsIm1hc3RlciJdfQ.R1aaKPsUh81_M_42BItgi_X7qC-nmoHz7X04PlzQn5A`,
   },
 });
 
