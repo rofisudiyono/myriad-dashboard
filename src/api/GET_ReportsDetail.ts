108e21a524809a301c87d1be0fb4cecebfd2b724
Rofi <rofisudyono@gmail.com>
2022-08-19 15:43:18 +0700

    remove proxy

--- src/api/GET_ReportsDetail.ts
+++ src/api/GET_ReportsDetail.ts
@@ -2,7 +2,7 @@ import axios from './axiosInstance';
 
 export const getReportsDetail = async ({id}: {id: string | undefined}) => {
   return axios
-    .get(`/api/reports/${id}/users`)
+    .get(`/reports/${id}/users`)
     .then(response => {
       return response.data;
     })
